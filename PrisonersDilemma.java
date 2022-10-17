084d96eeb8d7b8e86d48c8bbb9d6516cb29e8f60
SimsEmile <90972429+SimsEmile@users.noreply.github.com>
2022-10-17 20:36:35 +0200

    Add files via upload

--- PrisonersDilemma.java
+++ PrisonersDilemma.java
@@ -1,5 +1,6 @@
 import javax.swing.SwingUtilities;
-//...
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Prisoners Dilemma application.
@@ -13,7 +14,7 @@ import javax.swing.SwingUtilities;
  * @author NAME
  * @id ID
  */
-class PrisonersDilemma /* possible extends... */ {
+class PrisonersDilemma extends PlayingField {
     // ...
 
     /**
@@ -21,9 +22,22 @@ class PrisonersDilemma /* possible extends... */ {
      */
     void buildGUI() {
         SwingUtilities.invokeLater(() -> {
-            // ...
         });
-    }
+        JFrame frame = new JFrame("");
+            frame.setSize(800, 600);
+            JLabel underLabel = new JLabel("<html><h1>Go?</h1></html>");
+            underLabel.setBackground(new Color(255,255,255));
+            underLabel.setOpaque(true);
+            frame.add(underLabel,BorderLayout.SOUTH);
+            JPanel panel = new JPanel(new GridLayout(50, 50));
+            frame.add(panel);
+            
+            
+
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        }
 
     // ...
 
